fc6f6489f79bb11a3c577b8a4e59113263bdee57
Chris Corcinschi <chriscorcinschi.20@gmail.com>
2026-03-10 18:35:30 +0100

    feat: createContext & wrapped app  with CartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,22 @@ import { Layout } from "./components/ui/layout";
 import AuthProvider from "./context/AuthContext";
 import Navbar from "./components/Navbar";
 import ProductDetails from "./pages/ProductDetails";
+import CartProvider from "./context/CartContext";
 
 function App() {
    return (
       <AuthProvider>
-         <Navbar />
-         <Layout>
-            <Routes>
-               <Route path="/" element={<Home />} />
-               <Route path="/auth" element={<Auth />} />
-               <Route path="/checkout" element={<Checkout />} />
-               <Route path="/product/:id" element={<ProductDetails />} />
-            </Routes>
-         </Layout>
+         <CartProvider>
+            <Navbar />
+            <Layout>
+               <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/auth" element={<Auth />} />
+                  <Route path="/checkout" element={<Checkout />} />
+                  <Route path="/product/:id" element={<ProductDetails />} />
+               </Routes>
+            </Layout>
+         </CartProvider>
       </AuthProvider>
    );
 }
