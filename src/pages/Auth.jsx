85e084d9775aecd08feb524a233616321abfa8b6
Chris Corcinschi <chriscorcinschi.20@gmail.com>
2026-03-10 16:04:36 +0100

    feat: page Auth & logic

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { SimpleGrid } from "@chakra-ui/react";
+import { AuthCard } from "../components/AuthCard";
+import { useAuth } from "../context/AuthContext";
 
 export default function Auth() {
-   return <div>Auth</div>;
+   const [searchParams, setSearchParams] = useSearchParams();
+   const mode = searchParams.get("mode") === "login" ? "login" : "signup";
+
+   const [error, setError] = useState(null);
+   const [loading, setLoading] = useState(false);
+
+   const { signUp, login } = useAuth();
+   const navigate = useNavigate();
+
+   function handleToggleMode() {
+      setError(null);
+      setSearchParams({ mode: mode === "signup" ? "login" : "signup" });
+   }
+
+   async function handleSubmit({ email, password }) {
+      setError(null);
+      setLoading(true);
+
+      const result =
+         mode === "signup" ? signUp(email, password) : login(email, password);
+
+      setLoading(false);
+
+      if (result.success) {
+         navigate("/");
+      } else {
+         setError(result.error);
+      }
+   }
+
+   return (
+      <SimpleGrid justifyItems="center">
+         <AuthCard
+            mode={mode}
+            onSubmit={handleSubmit}
+            onToggleMode={handleToggleMode}
+            error={error}
+            loading={loading}
+         />
+      </SimpleGrid>
+   );
 }
