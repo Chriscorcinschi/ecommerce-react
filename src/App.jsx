77c5752345895e18bb5524421a28bcb14c46bf4e
Chris Corcinschi <chriscorcinschi.20@gmail.com>
2026-03-10 15:59:54 +0100

    fix: checkout name

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
+// App.jsx
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
-import Navbar from "./components/Navbar";
+
 import { Layout } from "./components/ui/layout";
+import AuthProvider from "./context/AuthContext";
+import Navbar from "./components/Navbar";
 
 function App() {
    return (
-      <>
+      <AuthProvider>
          <Navbar />
          <Layout>
             <Routes>
                <Route path="/" element={<Home />} />
                <Route path="/auth" element={<Auth />} />
-               <Route path="/chechout" element={<Checkout />} />
+               <Route path="/checkout" element={<Checkout />} />
             </Routes>
          </Layout>
-      </>
+      </AuthProvider>
    );
 }
 
